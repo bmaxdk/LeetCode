9347f80e3212bde7f5ab1d9c4eab4d6e40d68c80
bmaxdk <choh8uw@gmail.com>
2021-05-13 10:08:41 -0700

    Add p1 cpp file

---
+++ p1_twoSum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> m;
+        for(int i=0; i<nums.size(); i++) {
+            const int comp = target - nums[i];
+            if (m.count(comp) > 0) {
+                return vector<int> {i, m[comp]};
+            }
+            m[nums[i]] = i;
+        }
+        assert(false);
+    }
+};
