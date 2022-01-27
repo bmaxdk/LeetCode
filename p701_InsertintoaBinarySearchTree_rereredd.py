a136067c918f852c31a2d6dcb0e87277b5385350
bmaxdk <choh8uw@gmail.com>
2022-01-26 22:42:33 -0800

    Binary Search Tree

---
+++ p701_InsertintoaBinarySearchTree_rereredd.py
@@ -0,0 +1,20 @@
+# 701. Insert into a Binary Search Tree
+# https://leetcode.com/problems/insert-into-a-binary-search-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        #inital condition:
+        if not root:
+            return TreeNode(val)
+        
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right,val)
+            
+        else:
+            root.left = self.insertIntoBST(root.left,val)
+        return root
