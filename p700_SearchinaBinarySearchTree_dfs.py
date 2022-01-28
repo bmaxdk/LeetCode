0e490ac88c8f66d462e40cd979dc1bd36dc59c59
bmaxdk <choh8uw@gmail.com>
2022-01-27 17:37:57 -0800

    Binary Tree

---
+++ p700_SearchinaBinarySearchTree_dfs.py
@@ -0,0 +1,36 @@
+# 700. Search in a Binary Search Tree
+# https://leetcode.com/problems/search-in-a-binary-search-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val == val:
+                    return node
+                stack.append(node.right)
+                stack.append(node.left)
+        return 
+# bottom provide same but return type is different so rejected
+# class Solution:
+#     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+#         stack, res = [root], []
+#         while stack:
+#             node = stack.pop()
+#             if node:
+#                 if node.val == val:
+#                     res.append(node.val)
+#                     if node.left:
+#                         res.append(node.left.val)
+#                     if node.right:
+#                         res.append(node.right.val)
+#                     break
+#                 stack.append(node.right)
+#                 stack.append(node.left)
+#         return res
