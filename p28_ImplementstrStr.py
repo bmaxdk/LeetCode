d2e37b1483d45d7b35e2664496bd2bc63717217e
bmaxdk <choh8uw@gmail.com>
2022-01-30 18:08:49 -0800

    dp

---
+++ p28_ImplementstrStr.py
@@ -0,0 +1,7 @@
+# 28. Implement strStr()
+# https://leetcode.com/problems/implement-strstr/
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+        return haystack.index(needle)
